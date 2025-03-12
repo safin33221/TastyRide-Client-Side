8f7e3afcb5439fa6e0f86253b3330e3a1cfc8a1e
safin33221 <saifn33221@gmail.com>
2025-03-12 16:35:16 +0600

    completed to implements authenticaion Funciton

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
     </>
   );
   return (
-    <div className="navbar bg-base-100 shadow-2xl md:px-16 mx-auto  w-full">
+    <div className="navbar bg-base-100 shadow-2xl md:px-16 mx-auto  w-full fixed top-0 z-50">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
