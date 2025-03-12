b21c033dab59c0700734defafc5ae6d493044720
hayder <hayderbd4290@gmail.com>
2025-03-13 00:10:49 +0600

    login and register route creted

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import MainLayout from '../Layouts/MainLayout';
 import Home from '../Pages/Home/Home';
+import Login from '../Pages/Login/Login';
+import Register from '../Pages/Register/Register';
 
 const Router = () => {
   return (
@@ -9,6 +11,8 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+          <Route path='/register' element={<Register/>}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
