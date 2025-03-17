64ea5a9554b94af38930c7f2fa37e6cbc396ea37
safin33221 <saifn33221@gmail.com>
2025-03-17 20:52:21 +0600

    ADDED: setup tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,23 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import Router from './Router/Router.jsx';
 import AuthProvider from './Provider/AuthProvider.jsx';
+import {
+
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+// Create a client
+const queryClient = new QueryClient()
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AuthProvider>
-      <Router />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Router />
+      </AuthProvider>
+    </QueryClientProvider>
+
   </StrictMode>
 );
