fe1711c2a0862101d44da5ed206c8a7ce4df92db
hayder <hayderbd4290@gmail.com>
2025-03-22 19:32:09 +0600

    setup i18n and translate navbar menu

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,24 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import Router from './Router/Router.jsx';
-import AuthProvider from './Provider/AuthProvider.jsx';
-import {
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Router from "./Router/Router.jsx";
+import AuthProvider from "./Provider/AuthProvider.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n/i18n.js";
 
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
 
 // Create a client
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
-        <Router />
+        <I18nextProvider i18n={i18n}>
+          <Router />
+        </I18nextProvider>
       </AuthProvider>
     </QueryClientProvider>
-
   </StrictMode>
 );
