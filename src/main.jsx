80d68ac60582d6ebc192e52f597b1901efb0fbb8
hayder <hayderbd4290@gmail.com>
2025-03-22 23:09:36 +0600

    fix i18n issue

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,8 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n/i18n.js';
 
 // Create a client
 const queryClient = new QueryClient();
