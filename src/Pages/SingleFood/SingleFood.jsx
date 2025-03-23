9bb1ddc68baf2ad01d728d77f3b639d4d56743dd
khaironnahar4 <khaironnahar247@gmail.com>
2025-03-23 12:20:13 +0600

    UPDATE: updated signle food design dynamically using api.

--- src/Pages/SingleFood/SingleFood.jsx
+++ src/Pages/SingleFood/SingleFood.jsx
@@ -25,113 +25,74 @@ function SingleFood() {
 
   return (
     <section className="max-w-5xl mx-auto flex justify-center items-center h-screen">
-      <div className="card lg:card-side w-full shadow-sm">
+      <div className="card lg:card-side w-full bg-white shadow-sm">
         <div className="w-1/2 flex justify-center items-center p-6 md:p-8 lg:p-10">
           <div className="w-[350px] h-[350px] overflow-hidden rounded-lg">
             <img
-              src="https://rumkisgoldenspoon.com/wp-content/uploads/2021/05/Bhuna-khichuri-recipe.jpg"
-              alt="buna khichuri"
+              src={food?.image}
+              alt={`${food?.foodName} image`}
               className="w-full h-full object-cover"
             />
           </div>
         </div>
         <div className="card-body">
           <div className="card-body">
-            <span className="badge badge-xs badge-warning">Available</span>
+            <span className="badge badge-xs badge-warning">{food?.availability ? "Available" : "Not Available"}</span>
             <div className="flex justify-between">
               <div>
-                <h2 className="text-3xl font-bold">Buna Khichuri</h2>
-                <p className="font-semibold">Non-veg</p>
+                <h2 className="text-3xl font-bold">{food?.foodName}</h2>
+                <p className="font-semibold">{food?.category}</p>
               </div>
-              <span className="text-xl font-bold">290 tk</span>
+              <span className="text-xl font-bold">{food?.price} tk</span>
             </div>
             {/* description */}
             <div className="mt-4">
               <p>
-                Bhuna khichuri is a pure Bengali delicacy and it is also known
-                as bhuni khichuri. It is a traditional Bengali dish that is made
-                with rice, lentils, and vegetables. It is a very popular dish in
-                Bangladesh and West Bengal. Bhuna khichuri is a very healthy and
-                nutritious dish. It is a complete meal in itself.
+                {food?.description}
               </p>
             </div>
             {/* ingredients */}
             <h3 className="mt-6 text-xl font-bold">Ingredients</h3>
             <ul className="mt-2 flex flex-col gap-2 text-xs">
-              <li>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-4 me-2 inline-block text-success"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M5 13l4 4L19 7"
-                  />
-                </svg>
-                <span>Small grain rice</span>
-              </li>
-              <li>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-4 me-2 inline-block text-success"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M5 13l4 4L19 7"
-                  />
-                </svg>
-                <span>Split yellow lentils</span>
-              </li>
-              <li>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-4 me-2 inline-block text-success"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M5 13l4 4L19 7"
-                  />
-                </svg>
-                <span> fried onions</span>
-              </li>
-              <li>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-4 me-2 inline-block text-success"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M5 13l4 4L19 7"
-                  />
-                </svg>
-                <span>chicken</span>
-              </li>
+              {
+                food?.ingredients.map((ingredient, index) => (
+                  <li key={index}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-4 me-2 inline-block text-success"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M5 13l4 4L19 7"
+                    />
+                  </svg>
+                  <span>{ingredient}</span>
+                </li>
+                ))
+              }
+             
             </ul>
             {/* special tags  */}
             <div className="space-x-2 mt-4">
-              <div className="badge badge-outline badge-primary">Spicy</div>
-              <div className="badge badge-outline badge-secondary">Vegan</div>
-              <div className="badge badge-outline badge-info">Gluten Free</div>
+
+              {
+                food?.tags.map((tag, index) => {
+                  const tagColors = {
+                    Spicy: "badge-primary",
+                    Vegan: "badge-secondary",
+                    "Gluten-Free": "badge-info",
+                  };
+                  return (
+                    <div key={index} className={`badge badge-outline ${tagColors[tag]}`}>{tag}</div>
+                  )
+                }   
+                )
+              }
             </div>
             <div className="mt-6">
              <PrimaryButton text={"Order Now"} />
