85095c55206b25455cc904546d11efcf3778853f
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2025-04-02 13:43:47 +0600

    Updated: updated api routes

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-    // baseURL: window.location.pathname === "localhost" ? 'http://localhost:8000' : "https://tasty-ride-server-side.vercel.app"
-    baseURL: window.location.pathname === "localhost" ? 'http://localhost:8000' : "http://localhost:8000"
+    baseURL: window.location.pathname === "localhost" ? 'http://localhost:8000' : "https://tasty-ride-server-side.vercel.app"
+    // baseURL: window.location.pathname === "localhost" ? 'http://localhost:8000' : "http://localhost:8000"
 });
 
 const useAxiosPublic = () => {
