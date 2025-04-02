45a576ad456f1471fa600fad14ba5f9f90a86b44
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2025-04-02 13:45:09 +0600

    Fixed text overlay issues

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -8,11 +8,6 @@ import ChatBot from "../Components/ChatBot";
 
 const MainLayout = () => {
   const [openChat, setOpenChat] = useState(false);
-  let str = "Okay, I've reviewed the website content for TastyRide. Here's a summary of what I see:\n\n**Overall Impression:**\n\n*   TastyRide appears to be a platform for ordering international cuisine. The site highlights a variety of different food options and"
-
-let formattedStr = str.replace(/\*\*(.*?)\*\*/g, "<h1 className='font-bold'>$1</h1>");
-
-console.log(formattedStr)
   return (
     <div className="relative">
       <nav>
@@ -21,7 +16,6 @@ console.log(formattedStr)
         </Headroom>
       </nav>
       <main className=" min-h-[calc(100vh-65px)] mx-auto">
-        <p dangerouslySetInnerHTML={{__html:formattedStr}}/>
         <Outlet />
       </main>
       <footer>
