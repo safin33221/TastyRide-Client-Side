537a5e3116b99f93156b9b67850b85b23948717a
khaironnahar4 <khaironnahar247@gmail.com>
2025-04-08 12:01:22 +0600

    ADDED: added the restaurant profile in  single food page

--- src/Pages/SingleFood/SingleFood.jsx
+++ src/Pages/SingleFood/SingleFood.jsx
@@ -3,7 +3,13 @@ import PrimaryButton from "../../Shared/PrimaryButton";
 import { useNavigate, useParams, Link } from "react-router";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import useAxiosPublic from "../../Hooks/useAxiosPublic";
-import { FaArrowLeft, FaHeart, FaRegHeart, FaThumbsDown, FaThumbsUp } from "react-icons/fa";
+import {
+  FaArrowLeft,
+  FaHeart,
+  FaRegHeart,
+  FaThumbsDown,
+  FaThumbsUp,
+} from "react-icons/fa";
 import useUserData from "../../Hooks/useUserData";
 import { useAddToCart } from "../../Hooks/userAddToCart";
 
@@ -13,7 +19,7 @@ function SingleFood() {
   const axiosPublic = useAxiosPublic();
   const queryClient = useQueryClient();
   const navigate = useNavigate();
-  const addToCart = useAddToCart()
+  const addToCart = useAddToCart();
 
   const {
     data: food,
@@ -47,10 +53,10 @@ function SingleFood() {
   // manage the add to favorite functionality state
   const [isFavorited, setIsFavorited] = useState(false);
 
-  // add to cart function 
+  // add to cart function
   const handleAddToCart = (food) => {
-    addToCart(food)
-  }
+    addToCart(food);
+  };
 
   if (isLoading) return <p>Loading...</p>;
 
@@ -58,17 +64,15 @@ function SingleFood() {
 
   const toggleFavorite = () => setIsFavorited(!isFavorited);
 
-
-
   return (
     <section className="max-w-5xl mx-auto flex justify-center items-center h-screen">
       <div className="card lg:card-side w-full bg-white shadow-sm">
-       {/* Back Button */}
-       <button
+        {/* Back Button */}
+        <button
           className="absolute top-4 left-4 text-gray-600 hover:text-gray-800 text-xl flex items-center gap-2 cursor-pointer"
           onClick={() => navigate(-1)}
         >
-          <FaArrowLeft /> 
+          <FaArrowLeft />
         </button>
 
         {/* Love Icon */}
@@ -83,33 +87,33 @@ function SingleFood() {
         <div className="w-1/2 flex justify-center items-center p-6 md:p-8 lg:p-10">
           <div className="w-[350px] h-[350px] overflow-hidden rounded-lg">
             <img
-              src={food?.image}
-              alt={`${food?.foodName} image`}
+              src={food?.food?.image}
+              alt={`${food?.food?.foodName} image`}
               className="w-full h-full object-cover"
             />
           </div>
         </div>
-        {/* food details */}
+        {/* food dfood?.etails */}
         <div className="card-body">
           <div className="card-body">
             <span className="badge badge-xs badge-warning">
-              {food?.availability ? "Available" : "Not Available"}
+              {food?.food?.availability ? "Available" : "Not Available"}
             </span>
             <div className="flex justify-between">
               <div>
-                <h2 className="text-3xl font-bold">{food?.foodName}</h2>
-                <p className="font-semibold">{food?.category}</p>
+                <h2 className="text-3xl font-bold">{food?.food?.foodName}</h2>
+                <p className="font-semibold">{food?.food?.category}</p>
               </div>
-              <span className="text-xl font-bold">{food?.price} tk</span>
+              <span className="text-xl font-bold">{food?.food?.price} tk</span>
             </div>
             {/* description */}
             <div className="mt-4">
-              <p>{food?.description}</p>
+              <p>{food?.food?.description}</p>
             </div>
             {/* ingredients */}
             <h3 className="mt-6 text-xl font-bold">Ingredients</h3>
             <ul className="mt-2 flex flex-col gap-2 text-xs">
-              {food?.ingredients.map((ingredient, index) => (
+              {food?.food?.ingredients.map((ingredient, index) => (
                 <li key={index}>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -131,7 +135,7 @@ function SingleFood() {
             </ul>
             {/* special tags  */}
             <div className="space-x-2 mt-4">
-              {food?.tags.map((tag, index) => {
+              {food?.food?.tags.map((tag, index) => {
                 const tagColors = {
                   Spicy: "badge-primary",
                   Vegan: "badge-secondary",
@@ -152,21 +156,49 @@ function SingleFood() {
             <div className="mt-6 flex items-center gap-4">
               <button
                 className={`btn btn-sm btn-success btn-outline flex items-center gap-2`}
-                onClick={() => { reactToFood({ reaction: "like" }) }}
+                onClick={() => {
+                  reactToFood({ reaction: "like" });
+                }}
               >
-                <FaThumbsUp /> {food?.likes.length}
+                <FaThumbsUp /> {food?.food?.likes.length}
               </button>
               <button
                 className={`btn btn-sm btn-error btn-outline flex items-center gap-2`}
-                onClick={() => { reactToFood({ reaction: "dislike" }) }}
+                onClick={() => {
+                  reactToFood({ reaction: "dislike" });
+                }}
               >
-                <FaThumbsDown /> {food?.dislikes.length}
+                <FaThumbsDown /> {food?.food?.dislikes.length}
               </button>
             </div>
 
             {/* order now button */}
-            <div onClick={() => handleAddToCart(food)} className="mt-6">
+            <div onClick={() => handleAddToCart(food?.food)} className="mt-6">
               <PrimaryButton text={"Order Now"} />
+            </div>
+
+            {/* Restaurant Info */}
+            <div className="flex items-center gap-4 mt-6">
+              <div className="avatar">
+                <Link to={`/restaurantProfile/${food?.food?.addedBy}`}>
+                  <div className="w-16 rounded-full ring ring-red-500 ring-offset-base-100 ring-offset-2">
+                    <img
+                      src={
+                        food?.restaurantProfile ||
+                        "https://placehold.co/150?text=profile"
+                      }
+                      alt={food?.restaurantName}
+                      className="rounded-full"
+                    />
+                  </div>
+                </Link>
+              </div>
+
+              <Link to={`/restaurantProfile/${food?.food?.addedBy}`}>
+              <h2 className="text-2xl font-bold text-red-500 hover:text-red-600 hover:underline">
+                {food?.restaurantName ? food?.restaurantName : "Restaurant Name"}
+              </h2>
+              </Link>
             </div>
             {/* <div className="mt-6 flex gap-2 items-center ">
               <Link to={`/profile/${userData?._id}`}><img src={userData?.restaurantDetails?.profilePhoto} className="w-12 h-12 rounded-full" alt="" /></Link>
