e2036dad293fe59f55e4ad55b21b62d4fd18c56a
safin33221 <safin33221@gmail.com>
2025-04-10 20:21:36 +0600

    ADDED: implement select location Modal for detacted user location

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from "react";
 import CategoryFoods from "../../Components/CategoryFoods/CategoryFoods";
 import EasyOrder from "../../Components/EasyOrder/EasyOrder";
 import ExploreRestaurant from "../../Components/ExploreRestaurant/ExploreRestaurant";
 import InterNationalFood from "../../Components/internatinalFood/InterNationalFood";
+import LocationModal from "../../Components/Location/LocationModal";
 import OurClient from "../../Components/OurClient/OurClient";
 import PopularCollection from "../../Components/PopularCollection/PopularCollection";
 import Sliders from "../../Components/Sliders";
@@ -10,10 +12,25 @@ import DiscountsProduct from "../../EidFeatures/DiscountProducts/DiscountsProduc
 import SectionDivider from "../../Shared/SectionDivider";
 
 const Home = () => {
+
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    // const isFirstTime = localStorage.getItem("hasVisited");
+    // console.log("isFirstTime:", isFirstTime); // Debugging
+    // if (isFirstTime) {
+    //   setShowModal(true);
+    //   localStorage.setItem("hasVisited", "true");
+    setShowModal(true)
+    // }
+  }, []);
+
+  // console.log("showModal:", showModal); // Debugging
   return (
     <div>
 
       <Sliders></Sliders>
+      {showModal && <LocationModal onClose={() => setShowModal(false)} />}
       <div className="container mx-auto">
         <SectionDivider></SectionDivider>
         {/* <DiscountsProduct /> */}
