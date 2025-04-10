b855d34930b83c962b118c3c9e99ffa15d21898c
khaironnahar4 <khaironnahar247@gmail.com>
2025-04-10 11:12:00 +0600

    UPDATE: updated the newletter modal desing

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import CategoryFoods from "../../Components/CategoryFoods/CategoryFoods";
 import EasyOrder from "../../Components/EasyOrder/EasyOrder";
 import ExploreRestaurant from "../../Components/ExploreRestaurant/ExploreRestaurant";
@@ -8,8 +9,46 @@ import Sliders from "../../Components/Sliders";
 import CountDown from "../../EidFeatures/CountDown";
 import DiscountsProduct from "../../EidFeatures/DiscountProducts/DiscountsProduct";
 import SectionDivider from "../../Shared/SectionDivider";
+import useAuth from "../../Hooks/useAuth";
+import useAxiosPublic from "../../Hooks/useAxiosPublic";
+import NewLetterModal from "../../Components/NewLetterModal/NewLetterModal";
 
 const Home = () => {
+  const {user} = useAuth();
+  const axiosPublic = useAxiosPublic();
+  const [showModal, setShowModal] = useState(false);
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  
+  useEffect(() => {
+    // Check if the user is already subscribed
+    const checkSubscription = async () => {
+
+        
+    
+        // if (!isSubscribed){
+        //   const timer = setTimeout(() => {
+        //     setShowModal(true);
+        //   }, 5000); // Show modal after 5 seconds
+        //   return () => clearTimeout(timer); // Cleanup the timer on unmount
+        // }
+        const timer = setTimeout(()=> {
+          const response = axiosPublic.get(`/api/subscribe/${user?.email}`);
+          // console.log(response.data);
+          // setIsSubscribed(response.data.isSubscribed);
+          if(!response?.data?.isSubscribed){
+            setShowModal(true);
+          }
+        }, 5000); // Show modal after 5 seconds
+        return () => clearTimeout(timer); // Cleanup the timer on unmount
+    };
+    // console.log(user?.email);
+    user?.email && checkSubscription();
+    
+  }, [ axiosPublic, user?.email]);
+
+  const handleOnClose = () => {
+    setShowModal(false);
+  };
   return (
     <div>
 
@@ -30,6 +69,10 @@ const Home = () => {
         <SectionDivider></SectionDivider>
         <OurClient></OurClient>
         <SectionDivider></SectionDivider>
+        {/* newletter modal */}
+        {
+          showModal && <NewLetterModal onClose={handleOnClose} />
+        }
       </div>
     </div>
   );
