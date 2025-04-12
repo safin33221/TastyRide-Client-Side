d944fddc08428f21906f724e5aeef4d83c3ee4c7
safin33221 <safin33221@gmail.com>
2025-04-12 20:17:53 +0600

    ADDED:Design total admin, users, restaurant and riders card in admin dashboard

--- src/Dashboard/Admin Dashboard/AdminDashboard.jsx
+++ src/Dashboard/Admin Dashboard/AdminDashboard.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const AdminDashboard = () => {
     return (
-        <div>
-            <h1>This is a Admin Dashboard page  </h1>
+        <div className='mt-4 px-3'>
+            <div className='grid grid-cols-4 gap-2 '>
+                <div className='border h-28 rounded-xl flex items-center justify-center'>Total Admin</div>
+                <div className='border h-28 rounded-xl flex items-center justify-center'>Total Users</div>
+                <div className='border h-28 rounded-xl flex items-center justify-center'>Total Restaurants</div>
+                <div className='border h-28 rounded-xl flex items-center justify-center'>Total Riders</div>
+            </div>
         </div>
     );
 };
