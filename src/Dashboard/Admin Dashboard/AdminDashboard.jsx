6a02de508006ba26e90e22a47c2a4c7c440fe6d1
safin33221 <safin33221@gmail.com>
2025-04-12 21:03:47 +0600

    ADDED:  Completed get user and show total admin , restaurant , customers and riders

--- src/Dashboard/Admin Dashboard/AdminDashboard.jsx
+++ src/Dashboard/Admin Dashboard/AdminDashboard.jsx
@@ -1,20 +1,44 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useAxiosPublic from '../../Hooks/useAxiosPublic';
 
 const AdminDashboard = () => {
+    const axiosPublic = useAxiosPublic()
+    const { data: users } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const userRes = await axiosPublic.get(`/api/users`)
+            return userRes.data
+        }
+
+    })
+    const TotalAdmin = users?.filter(user => user.role === 'admin')
+    const TotalCustomers = users?.filter(user => user.role === 'customer')
+    const TotalRestaurant = users?.filter(user => user.role === 'restaurant')
+    const TotalRiders = users?.filter(user => user.role === 'riders')
+
     return (
         <div className='mt-4 px-3 space-y-2'>
-            <div className='grid grid-cols-4 gap-2 '>
-                <div className='border h-28 rounded-xl flex items-center justify-center'>Total Admin</div>
-                <div className='border h-28 rounded-xl flex items-center justify-center'>Total Users</div>
-                <div className='border h-28 rounded-xl flex items-center justify-center'>Total Restaurants</div>
-                <div className='border h-28 rounded-xl flex items-center justify-center'>Total Riders</div>
+            <div className='grid grid-cols-2 md:grid-cols-4 gap-2 '>
+                <div className='border h-28 rounded-xl flex items-center justify-center text-xl bg-indigo-200 uppercase  font-bold text-center '>
+                     {TotalAdmin.length || 0} <br /> Admin
+                </div>
+                <div className='border h-28 rounded-xl flex items-center justify-center text-xl bg-indigo-200 uppercase  font-bold text-center '>
+                      {TotalCustomers?.length} <br /> Customers
+                </div>
+                <div className='border h-28 rounded-xl flex items-center justify-center text-xl bg-indigo-200 uppercase  font-bold text-center '>
+                     {TotalRestaurant?.length} <br /> Restaurants
+                </div>
+                <div className='border h-28 rounded-xl flex items-center justify-center text-xl bg-indigo-200 uppercase  font-bold text-center '>
+                     {TotalRiders?.length} <br /> Riders
+                </div>
             </div>
 
-            <div className='grid grid-cols-2 gap-2'>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-2'>
                 <div className='border h-28 rounded-xl flex items-center justify-center'>Total Foods</div>
                 <div className='border h-28 rounded-xl flex items-center justify-center'> Total Sales</div>
             </div>
-            <div className='grid grid-cols-2 gap-2'>
+            <div className='grid md:grid-cols-2 gap-2'>
                 <div className='border h-80 rounded-xl flex items-center justify-center'>Sales Overview by Date</div>
                 <div className='border h-80 rounded-xl flex items-center justify-center'> Pi cahrt Order Status Summary Pending Cooking On The Way Delivered Canceled</div>
             </div>
@@ -29,7 +53,7 @@ const AdminDashboard = () => {
             </div>
             <div className=' gap-2'>
                 <div className='border h-80 rounded-xl flex items-center justify-center'>Recent orders details</div>
-               
+
             </div>
         </div>
     );
