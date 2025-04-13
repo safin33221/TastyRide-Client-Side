7686df6b6e8c9cae04d418598b52cd73fbd543eb
safin33221 <safin33221@gmail.com>
2025-04-14 01:30:05 +0600

    Added: completed success and error handler for secure https request

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -6,6 +6,16 @@ const axiosSecure = axios.create({
     baseURL: window.location.hostname === 'localhost' ? 'http://localhost:8000' : "https://tasty-ride-server-side.vercel.app"
 })
 const useAxiosSecure = () => {
+    const { LogoutUser } = useAuth()
+    const navigate = useNavigate()
+    axiosSecure.interceptors.request.use(function (config) {
+        const token = localStorage.getItem('token')
+        config.headers.Authorization = `Bearer ${token}`
+        return config
+    }, function (error) {
+        return Promise.reject(error)
+    })
+
     
 
     return axiosSecure
