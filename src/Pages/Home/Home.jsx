0be739655ccbaaba0dcb3c5921f1c9acec33592d
tawhid <ahmedshanto690@gmail.com>
2025-05-01 13:12:25 +0600

    Comment out DiscountModal import and usage in Home component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -16,7 +16,7 @@ import Reviews from '../../Components/Reviews/Reviews';
 import NewsLetterModal from '../../Components/NewsLetterModal/NewsLetterModal';
 import DeliveryCities from '../../Components/DeliveryCities/DeliveryCities';
 import BecomePartner from '../../Components/LandingPage/BecomePartner';
-import DiscountModal from '../../Components/DiscountModal/DiscountModal';
+// import DiscountModal from '../../Components/DiscountModal/DiscountModal';
 
 const Home = () => {
   const { user } = useAuth();
@@ -75,7 +75,7 @@ const Home = () => {
       <div className="container mx-auto">
         <SectionDivider></SectionDivider>
         {/* <DiscountsProduct /> */}
-        <DiscountModal userEmail={user?.email} />
+        {/* <DiscountModal userEmail={user?.email} /> */}
         <ExploreRestaurant />
         <SectionDivider></SectionDivider>
         <BecomePartner></BecomePartner>
