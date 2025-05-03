da8dee64793100dae82345f669456d69e34b9140
shofiq18 <shafiqul.islam25021998@gmail.com>
2025-05-03 16:46:25 +0600

    Enhance SectionTitle component: update description text color for better readability

--- src/Shared/SectionTitle.jsx
+++ src/Shared/SectionTitle.jsx
@@ -6,7 +6,7 @@ const SectionTitle = ({ title, desc }) => {
       <h1 className="text-center text-2xl md:text-4xl font-semibold mb-3">
         {title}
       </h1>
-      <p className="text-center text-gray-400 font-medium mb-12">
+      <p className="text-center text-gray-600 font-medium mb-12">
         {desc}
       </p>
     </div>
