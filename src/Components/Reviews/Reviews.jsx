4df91f6a7b44a8211024fdb2db923c37a7c1a0a8
shofiq18 <shafiqul.islam25021998@gmail.com>
2025-05-03 16:31:05 +0600

    Refactor Reviews component: update styling for review cards and remove commented-out code

--- src/Components/Reviews/Reviews.jsx
+++ src/Components/Reviews/Reviews.jsx
@@ -63,7 +63,7 @@ const Reviews = () => {
         >
           <div className="flex gap-6">
             {reviews.map(review => (
-              <div key={review._id} class="min-w-xs max-w-xs mx-auto bg-white shadow-lg rounded-xl p-4 text-center relative">
+              <div key={review._id} class="min-w-xs max-w-xs  mx-auto bg-gray-100 shadow-lg rounded-bl-4xl rounded-tr-4xl p-4 text-center relative">
                 {/* <!-- Customer Image --> */}
                 <div class="absolute -top-10 left-1/2 transform -translate-x-1/2">
                   <div class="w-20 h-20 z-20 rounded-full border-4 border-red-500 overflow-hidden shadow-md">
@@ -108,38 +108,6 @@ const Reviews = () => {
                 </div>
               </div>
 
-              // <div
-              //   key={review._id}
-              //   className="min-w-[250px] bg-gray-100 font-semibold shadow rounded-lg p-4 border border-gray-100"
-              // >
-              //   <div className="flex items-center mb-2">
-              //     <div className="bg-blue-100 text-blue-600 rounded-full w-10 h-10 flex items-center justify-center font-bold mr-3">
-              //       {review.userId.charAt(0).toUpperCase()}
-              //     </div>
-              //     <div className="text-sm text-gray-700">
-              //       {review.userId.split('@')[0]}
-              //     </div>
-              //   </div>
-              // <div className="flex items-center mb-2">
-              //   {[1, 2, 3, 4, 5].map(star => (
-              //     <svg
-              //       key={star}
-              //       className={`w-4 h-4 ${star <= review.rating
-              //           ? 'text-yellow-400'
-              //           : 'text-gray-300'
-              //         }`}
-              //       fill="currentColor"
-              //       viewBox="0 0 20 20"
-              //     >
-              //       <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
-              //     </svg>
-              //   ))}
-              // </div>
-              //   <p className="text-gray-600 text-sm">{review.review}</p>
-              //   <p className="text-xs text-gray-400 mt-2">
-              //     {new Date(review.createdAt).toLocaleDateString()}
-              //   </p>
-              // </div>
             ))}
           </div>
         </Marquee>
